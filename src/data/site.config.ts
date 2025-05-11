d924aa2ce1af673a5b0aed4cd28c39cfa3436f1b
조재호 <jaeho@jojaehoui-MacBookPro-2.local>
2025-05-12 06:23:04 +0900

    add category

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,8 +11,8 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'Realkor', // Site author
+	title: 'Realkor', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
